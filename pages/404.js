6ef59239fa0f5ed61dca69ead6949ae10e27a1ad
RounakSoni <soni.rounak0713@gmal.com>
2021-04-08 20:38:04 +0530

    404 & contact

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,7 @@
 const NotFound = () => {
     return (
-        <div>
-            <h1>What are you looking for ?</h1>
+        <div style={{"height:100vh;background-image=url('https://user-images.githubusercontent.com/46385227/114036312-990d3880-989d-11eb-8095-92ea9b7087ea.jpg');background-repeat:no-repeat;background-size:cover"}}>
+            <h1>What are you looking for?</h1>
         </div>
     );
 }
