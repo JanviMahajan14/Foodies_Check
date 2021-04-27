cee0822dca7217326b64e5cc17f81b0473a33bc2
JanviMahajan14 <janvimahajan337@gmail.com>
2021-04-27 20:46:55 +0530

    Fixed cart issue

--- models/cart.js
+++ models/cart.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-const {ObjectId} = mongoose.Schema.Types
+const { ObjectId } = mongoose.Schema.Types
 
 const cartSchema  = new mongoose.Schema({
     user:{
--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,7 +1,7 @@
 import initDB from "../../utils/initDB";
 import User from "../../models/user";
 import bcrypt from "bcrypt"
-import cart from "../../models/cart"
+import Cart from "../../models/cart"
 
 initDB();
 
@@ -10,22 +10,22 @@ export default async (req, res) => {
         const { name, email, password } = req.body;
         if (!name || !email || !password) {
             return res.status(400).send({ error: "Please fill all the fields" })
-    }
-    const user = await User.findOne({ email })
-    if (user) {
-        return res.status(400).send({ error: "User already exists" });
         }
-    const hashedPassword = await bcrypt.hash(password, 8);
-    const newUser = await new User({
-        name,
-        email,
-        password:hashedPassword
-        }).save()
+        const user = await User.findOne({ email })
+        if (user) {
+            return res.status(400).send({ error: "User already exists" });
+        }
+        const hashedPassword = await bcrypt.hash(password, 8);
+        const newUser = await new User({
+            name,
+            email,
+            password:hashedPassword
+            }).save()
+        await new Cart({ user: newUser._id }).save()
         res.send(newUser)
-        await new Cart({user:newuser. id}).save()
     }
     catch (error) {
-      res.statusCode = 400;
-      res.send({ error: error.message });
+        res.statusCode = 400;
+        res.send({ error: error.message });
     }
 }
